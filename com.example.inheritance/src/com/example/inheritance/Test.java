1474b40b52241f05c4a8c032a056d9d78d485d5d
Rituraj <GOVIND KUMAR@192.168.2.13>
2016-10-11 21:11:13 +0530

    bcbjksdc

--- com.example.inheritance/src/com/example/inheritance/Test.java
+++ com.example.inheritance/src/com/example/inheritance/Test.java
@@ -2,12 +2,15 @@ package com.example.inheritance;
 
 public class Test {
 	public void methodOne(String args){
+		//added comment here
 		System.out.println("string args");
 	}
 	public void methodOne(Object ob){
 		System.out.println("Object args");
 	}
 	public static void main(String[] args) {
+		
+		// added second comment
 		Test t = new Test();
 		t.methodOne(new Object());
 		t.methodOne("Bhaskar");
